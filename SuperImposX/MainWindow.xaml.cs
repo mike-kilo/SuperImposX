e1f91fe194ae902128fde2bd464881f2803d829b
Mike K <rocker@gazeta.pl>
2022-04-20 21:58:39 +0200

    Add track canvas controls and draw the track according to them

--- SuperImposX/MainWindow.xaml.cs
+++ SuperImposX/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,6 +31,52 @@ namespace SuperImposX
         public static readonly DependencyProperty CurrentFileProperty =
             DependencyProperty.Register("CurrentFile", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
 
+        public Size TrackCanvasSize
+        {
+            get { return (Size)GetValue(TrackCanvasSizeProperty); }
+            set 
+            { 
+                SetValue(TrackCanvasSizeProperty, value);
+                this.TrackCanvas.UpdateLayout();
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for TrackCanvasSize.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TrackCanvasSizeProperty =
+            DependencyProperty.Register("TrackCanvasSize", typeof(Size), typeof(MainWindow), new PropertyMetadata(new Size() { Width = 100.0, Height = 100.0 }));
+
+        public double TrackCanvasWidth
+        {
+            get { return (double)GetValue(TrackCanvasWidthProperty); }
+            set 
+            { 
+                SetValue(TrackCanvasWidthProperty, value);
+                if (_trackPoints == null) return;
+                var bounds = _trackPoints.GetBounds();
+                var aspect = (bounds.Max.Longitude - bounds.Min.Longitude) / (bounds.Max.Latitude - bounds.Min.Latitude);
+                SetCanvasSize(this.TrackCanvas, new Size() { Width = value, Height = value / aspect });
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for size.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TrackCanvasWidthProperty =
+            DependencyProperty.Register("TrackCanvasWidth", typeof(double), typeof(MainWindow), new PropertyMetadata(100.0));
+
+        public Point TrackCanvasOrigin
+        {
+            get { return (Point)GetValue(TrackCanvasOriginProperty); }
+            set 
+            { 
+                SetValue(TrackCanvasOriginProperty, value);
+                Canvas.SetLeft(this.TrackCanvas, value.X);
+                Canvas.SetTop(this.TrackCanvas, value.Y);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for TrackCanvasOrigin.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TrackCanvasOriginProperty =
+            DependencyProperty.Register("TrackCanvasOrigin", typeof(Point), typeof(MainWindow), new PropertyMetadata(new Point() { X = 0, Y = 0}));
+
         private static IEnumerable<GPXProcessing.TrackPoint>? _trackPoints;
 
         public MainWindow()
@@ -54,6 +100,16 @@ namespace SuperImposX
             canvas.Children.Add(line);
         }
 
+        private void RedrawTrackCanvas()
+        {
+            if (_trackPoints == null) return;
+
+            var bounds = _trackPoints.GetBounds();
+            var aspect = (bounds.Max.Latitude - bounds.Min.Latitude) / (bounds.Max.Longitude - bounds.Min.Longitude);
+            SetCanvasSize(this.TrackCanvas, new Size() { Width = this.TrackCanvasWidth, Height = this.TrackCanvasWidth * aspect });
+            SetCanvasOrigin(this.TrackCanvas, this.TrackCanvasOrigin);
+            DrawGPXOnCanvas(_trackPoints, this.TrackCanvas);
+        }
 
         public static void SetCanvasSize(Canvas canvas, Size size)
         {
@@ -83,7 +139,12 @@ namespace SuperImposX
             this.CurrentFile = ofd.FileName;
 
             _trackPoints = GPXProcessing.ReadGPX(this.CurrentFile);
-            DrawGPXOnCanvas(_trackPoints, this.PreviewCanvas);
+            this.RedrawTrackCanvas();
+        }
+
+        private void RedrawClick(object sender, RoutedEventArgs e)
+        {
+            this.RedrawTrackCanvas();
         }
     }
 }
