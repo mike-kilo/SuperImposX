b935866e4e0cf27b28642dda69a84f135e3e0ada
Mike K <rocker@gazeta.pl>
2022-05-27 19:38:19 +0200

    Add value converter

---
+++ SuperImposX/ValueConverters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SuperImposX.ValueConverters
+{
+    internal class BooleanToVisibilityConverter : IValueConverter
+    {
+        #region Constants
+        
+        private const string Message = "The target must be a visibility";
+
+        #endregion
+
+        #region IValueConverter Properties
+
+        public Visibility TrueValue { get; set; }
+        public Visibility FalseValue { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public BooleanToVisibilityConverter()
+        {
+            // set defaults
+            FalseValue = Visibility.Hidden;
+            TrueValue = Visibility.Visible;
+        }
+
+        #endregion
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(Visibility))
+                throw new InvalidOperationException(Message);
+
+            return (bool)value ? this.TrueValue : this.FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
